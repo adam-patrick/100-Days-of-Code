1699fc382ad7a2a38f81aea426241444c0049215
Adam Patrick <98496625+adam-patrick@users.noreply.github.com>
2022-04-27 13:14:00 -0500

    Add files via upload

---
+++ Day 30 Practice/note_storage.py
@@ -0,0 +1,15 @@
+# # FileNotFound Error
+# with open("a_file.txt") as file:
+#      file.read()
+
+# KeyError
+# a_dictionary = {"key", "value"}
+# value = a_dictionary["non-existent key"]
+
+# IndexError
+# fruit_list = {"Apple", "Banana", "Pear"}
+# fruit = fruit_list[3]
+
+# TypeError
+# text = "abc"
+# print(text + 5)
